c4847cb4d469d33cb2b66b85451c91519a3fd032
hhirai0902 <S016037>
2018-08-30 16:54:28 +0900

    step3 日経HPの日経平均株価を1度だけ取得する

--- scraping/getNikkeiWebPageTitle.py
+++ scraping/getNikkeiWebPageTitle.py
@@ -12,14 +12,34 @@ html = urllib.request.urlopen(url)
 # htmlをBeautifulSoupで扱う
 soup = BeautifulSoup(html, "html.parser")
 
-# タイトル要素を取得する → <title>経済、株価、ビジネス、政治のニュース:日経電子版</title>
-title_tag = soup.title
+# span要素全てを摘出する→全てのspan要素が配列に入ってかえされます
+# →[<span class="m-wficon triDown"></span>, <span class="l-miH02_H02c_btn_text">International</span>...
+# div:あらゆるものを挟んで大きなまとまりを作る、display:block
+# span:フレーズをグループ化する、グループしたフレーズはCSS（色設定など）を一括でできる、display:inline
+# class:divやspanに付ける名前
+span = soup.find_all("span")
 
-# 要素の文字列を取得する → 経済、株価、ビジネス、政治のニュース:日経電子版
-title = title_tag.string
+# print時のエラーとならないように最初に宣言しておきます
+nikkei_heikin = ""
 
-# タイトル要素を出力
-#print(title_tag)
+# for分で全てのspan要素の中からClass="mkc-stock_prices"となっている物を探します
+for tag in span:
+    # classの設定がされていない要素は、tag.get("class").pop(0)を行うことのできないでエラーとなるため、tryでエラーを回避する
+    try:
+        # tagの中からclass="n"のnの文字列を摘出します。複数classが設定されている場合があるので
+        # get関数では配列で帰ってくる。そのため配列の関数pop(0)により、配列の一番最初を摘出する
+        # <span class="hoge" class="foo">  →   ["hoge","foo"]  →   hoge
+        string_ = tag.get("class").pop(0)
 
-# タイトルを文字列を出力
-print(title)
+        # 摘出したclassの文字列にmkc-stock_pricesと設定されているかを調べます
+        if string_ in "mkc-stock_prices":
+            # mkc-stock_pricesが設定されているのでtagで囲まれた文字列を.stringであぶり出します
+            nikkei_heikin = tag.string
+            # 摘出が完了したのでfor分を抜けます
+            break
+    except:
+        # パス→何も処理を行わない
+        pass
+
+# 摘出した日経平均株価を出力します。
+print(nikkei_heikin)
