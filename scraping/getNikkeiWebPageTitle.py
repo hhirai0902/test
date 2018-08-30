b241bd040c4343a0c6c6c2275035c0a486144557
hhirai0902 <S016037>
2018-08-30 15:44:35 +0900

    step2

---
+++ scraping/getNikkeiWebPageTitle.py
@@ -0,0 +1,25 @@
+# coding: UTF-8
+#import urllib2
+import urllib.request, urllib.error
+from bs4 import BeautifulSoup
+
+# アクセスするURL
+url = "https://www.nikkei.com/markets/kabu/"
+
+# URLにアクセスする htmlが帰ってくる → <html><head><title>経済、株価、ビジネス、政治のニュース:日経電子版</title></head><body....
+html = urllib.request.urlopen(url)
+
+# htmlをBeautifulSoupで扱う
+soup = BeautifulSoup(html, "html.parser")
+
+# タイトル要素を取得する → <title>経済、株価、ビジネス、政治のニュース:日経電子版</title>
+title_tag = soup.title
+
+# 要素の文字列を取得する → 経済、株価、ビジネス、政治のニュース:日経電子版
+title = title_tag.string
+
+# タイトル要素を出力
+#print(title_tag)
+
+# タイトルを文字列を出力
+print(title)
