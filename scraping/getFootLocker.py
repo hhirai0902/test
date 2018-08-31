825c533f22c4512fa172bc57e7d7009ca8be3cd1
hhirai0902 <h_hirai0902@yahoo.co.jp>
2018-08-31 12:13:14 +0900

    CSSセレクター https://qiita.com/Azunyan1111/items/b161b998790b1db2ff7a

--- scraping/getFootLocker.py
+++ scraping/getFootLocker.py
@@ -15,25 +15,27 @@ import uuid                             # ユニークなIDを生成
 # 初期値設定
 # ------------------------------------ #
 # アクセスするURL
-url = "https://www.footlocker.com/product/nike-free-m-2018--mens/42836008.html"
+url = "https://www.nikkei.com/markets/kabu/"
+#url = "https://www.footlocker.com/product/nike-free-m-2018--mens/42836008.html"
 #url = "https://search.nifty.com/imagesearch/search?select=1&q=%s&ss=up"
-keyword = "猫"
+#keyword = "猫"
 
-# 取得画像格納
-images = []
 
 
 # ------------------------------------ #
 # スクレイピング処理
 # ------------------------------------ #
-# URLにアクセスする htmlが帰ってくる → <html><head><title>経済、株価、ビジネス、政治のニュース:日経電子版</title></head><body....
-#html = urllib.request.urlopen(url%(keyword))
-r = requests.get(url)
+# URLにアクセスする 戻り値にはアクセスした結果やHTMLなどが入ったinstanceが帰ってきます
+instance = urllib.request.urlopen(url)
 
 # htmlをBeautifulSoupで扱う,URL内を解析
-#soup = BeautifulSoup(html, "html.parser")
-soup = BeautifulSoup(r.content, "lxml")
+soup = BeautifulSoup(instance, "html.parser")
 
+# CSSセレクターを使って指定した場所のtextを表示します
+print(soup.select_one("#CONTENTS_MARROW > div.mk-top_stock_average.cmn-clearfix > div.cmn-clearfix > div.mkc-guidepost > div.mkc-prices > span.mkc-stock_prices").text)
+
+
+'''
 # span要素全てを摘出する→全てのspan要素が配列に入ってかえされます
 imgs = soup.find_all("img")
 
@@ -55,3 +57,4 @@ for img in imgs:
     with open(str("./picture/") + str(uuid.uuid4()) + str(".jpg"), "wb") as file:
         file.write(r.content)
 
+'''
